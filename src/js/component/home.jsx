fc90aab81e6e173f4d1b33f841c833aefb85d0c0
Allan Roger <105656076+allanrogerfl@users.noreply.github.com>
2024-01-11 21:27:17 +0000

    primecommit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
+	const [todos, setTodos] = useState(["Eat", "Make the bed"]);
+	const [newTodo, setNewTodo] = useState("");
+
+	const addTodo = (e) => {
+		e.preventDefault();
+		setTodos([...todos, newTodo]);
+		setNewTodo("");
+	};
+
+	const removeTodo = (index) => {
+		setTodos(todos.filter((_, todoIndex) => todoIndex !== index));
+	};
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container">
+			<h1>Todos</h1>
+			<form onSubmit={addTodo}>
+				<ul className="list-group">
+					<li className="list-group-item"><input
+						type="text"
+						placeholder="What do you need to do?"
+						value={newTodo}
+						onChange={(e) => setNewTodo(e.target.value)}
+					/>
+						<button className="btnAdd" type="submit"></button>
+					</li>
+					{todos.map((todo, index) => (
+						<li className="list-group-item" key={index}>
+							{todo} <button className="btnRemove" onClick={() => removeTodo(index)}>X</button>
+						</li>
+					))}
+					<li className="list-group-item">
+						<p className="itemCounter">{todos.length} item left</p>
+					</li>
+				</ul>
+			</form>
 		</div>
 	);
 };
 
 export default Home;
+
